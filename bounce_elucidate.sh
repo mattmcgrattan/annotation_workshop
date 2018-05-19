4da410a1900db863f28f85b554950ee4eb2b103f
Matt McGrattan <matt.mcgrattan@digirati.com>
2018-05-19 18:09:15 +0100

    Added 'pull' and force-create

--- bounce_elucidate.sh
+++ bounce_elucidate.sh
@@ -2,5 +2,5 @@ docker kill washington_annotation-database_1
 docker rm washington_annotation-database_1
 docker kill washington_annotation-server_1
 docker rm washington_annotation-server_1
-
-docker-compose up --build -d
+docker-compose pull
+docker-compose up --build -d --force-recreate
