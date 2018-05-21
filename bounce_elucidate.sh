c637cfd550c2420446f1248ce1d70fd899edb7cb
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2018-05-21 11:37:26 -0400

    Changed container names

--- bounce_elucidate.sh
+++ bounce_elucidate.sh
@@ -1,7 +1,7 @@
-docker kill washington_annotation-database_1
-docker rm washington_annotation-database_1
-docker kill washington_annotation-server_1
-docker rm washington_annotation-server_1
+docker kill annotation_workshop_annotation-database_1
+docker rm annotation_workshop_annotation-database_1
+docker kill annotation_workshop_annotation-server_1
+docker rm annotation_workshop_annotation-server_1
 docker-compose rm -v
 docker-compose pull
 docker-compose up --build -d --force-recreate
