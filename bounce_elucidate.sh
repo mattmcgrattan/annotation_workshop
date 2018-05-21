4d31b8998dde24aad9d24590311d556e3c103698
Matt McGrattan <matt.mcgrattan@digirati.com>
2018-05-21 10:15:24 -0400

    Remove anonymous volumes

--- bounce_elucidate.sh
+++ bounce_elucidate.sh
@@ -2,5 +2,6 @@ docker kill washington_annotation-database_1
 docker rm washington_annotation-database_1
 docker kill washington_annotation-server_1
 docker rm washington_annotation-server_1
+docker-compose rm -v
 docker-compose pull
 docker-compose up --build -d --force-recreate
